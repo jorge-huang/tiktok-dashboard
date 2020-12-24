e48843b135138e30d4d7cc023f3c5ebb68b1d433
Jorge Huang <jorgehuang27@gmail.com>
2020-12-24 15:48:19 -0500

    Models for TikTok and Browser

---
+++ models/browser.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+class Browser:
+    def __init__(self):
+        options = Options()
+        options.headless = True
+        self._browser = webdriver.Firefox(options=options)
+
+    def __navigate_to(self, url):
+        self._browser.get(url)
+
+    def get_html_by_class_name(self, url, class_name):
+        self.__navigate_to(url)
+        el = self._browser.find_element_by_class_name(class_name)
+        return el.get_attribute('innerHTML')
---
+++ models/tiktok.py
@@ -0,0 +1,33 @@
+from time import time
+from .browser import Browser
+import re
+
+class TikTok:
+    def __init__(self, account_handle):
+        self.account_handle = account_handle
+        self.followers = 0
+        self.likes = 0
+        self._last_update = float('-inf')
+        self._cache = None
+        self._browser = Browser()
+
+    def __update_cache(self):
+        if not self._cache or (time() - self._last_update) > 60:
+            url = f'https://www.tiktok.com/{self.account_handle}'
+            txt = self._browser.get_html_by_class_name(url, 'count-infos')
+            fields = ['Following', 'Followers', 'Liles']
+            vals = re.findall("\d+", txt)
+            self._cache = tuple(zip(fields, vals))
+            self._last_update = time()
+            print('### Cache updated ###')
+
+    def _update_followers(self):
+        self.followers = self._cache[1][1]
+
+    def _update_likes(self):
+        self.likes = self._cache[2][1]
+
+    def refresh_data(self):
+        self.__update_cache()
+        self._update_followers()
+        self._update_likes()
