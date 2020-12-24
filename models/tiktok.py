5d188ed3be15c4e37ad92ceb30455194c8a5d894
Jorge Huang <jorgehuang27@gmail.com>
2020-12-24 18:44:37 -0500

    Refactor cache update

--- models/tiktok.py
+++ models/tiktok.py
@@ -1,27 +1,28 @@
 from time import time
 from .browser import Browser
-import re
 
 class TikTok:
     def __init__(self, account_handle):
         self.account_handle = account_handle
         self.followers = 0
         self.likes = 0
-        self._last_update = float('-inf')
-        self._cache = None
+        self._last_update = time()
+        self._cache = []
         self._browser = Browser()
         self.REFRESH_RATE = 60 / 4
 
     def __update_cache(self):
-        if not self._cache or (time() - self._last_update) > self.REFRESH_RATE:
-            url = f'https://www.tiktok.com/{self.account_handle}'
-            txt = self._browser.get_inner_text_by_class_name(url, 'count-infos')
-            vals = txt.split('\n')
-            self._cache = []
-            for i in range(0, len(vals), 2):
-                temp = [vals[i + 1], vals[i]]
-                self._cache.append(temp)
-            self._last_update = time()
+        if (time() - self._last_update) < self.REFRESH_RATE:
+            return
+
+        url = f'https://www.tiktok.com/{self.account_handle}'
+        txt = self._browser.get_inner_text_by_class_name(url, 'count-infos')
+        vals = txt.split('\n')
+        self._cache = []
+        for i in range(0, len(vals), 2):
+            temp = [vals[i + 1], vals[i]]
+            self._cache.append(temp)
+        self._last_update = time()
 
     def _update_followers(self):
         self.followers = self._cache[1][1]
@@ -30,6 +31,9 @@ class TikTok:
         self.likes = self._cache[2][1]
 
     def refresh_data(self):
-        self.__update_cache()
-        self._update_followers()
-        self._update_likes()
+        try:
+            self.__update_cache()
+            self._update_followers()
+            self._update_likes()
+        except Exception as e:
+            print('FAILED TO REFRESH CACHE', e)
