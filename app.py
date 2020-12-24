9927a654afe2fb95da5c6bd0e1e65457e3220f1a
Jorge Huang <jorgehuang27@gmail.com>
2020-12-24 15:48:40 -0500

    Creating socket for followers and likes

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+from models import TikTok
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+# TODO: replace with env variable ACC_HANDLE
+tiktok = TikTok('{account handle}')
 
+
+def emit_data():
+    data = {
+        'followers': tiktok.followers,
+        'likes': tiktok.likes
+    }
+    socketio.emit('data', data)
 
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
 
+@socketio.on('connect')
+def on_connect():
+    emit_data()
+
+@socketio.on('refresh_data')
+def on_refresh_data():
+    tiktok.refresh_data()
+    emit_data()
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    socketio.run(app)
