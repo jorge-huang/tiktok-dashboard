4318a2f05834dab79289c7e5cacabaf2911e66ca
Jorge Huang <jorgehuang27@gmail.com>
2020-12-24 22:02:15 -0500

    Add delta from previous to current values in tables

--- static/index.js
+++ static/index.js
@@ -4,16 +4,34 @@ function createTd(val) {
     return tdEl;
 }
 
-function updateTable(data) {
+function calcValueDelta(curr, prev) {
+    curr = parseInt(curr);
+    prev = parseInt(prev);
+    let res = curr - prev;
+
+    if (curr > prev) {
+        return '+' + res;
+    }
+
+    if (curr < prev) {
+        return '-' + res;
+    }
+
+    return res;
+}
+
+function updateTable(data, prevFollowers, prevLikes) {
     const trEl = document.createElement('tr');
-    trEl.appendChild(createTd(new Date()))
+    trEl.appendChild(createTd(new Date().toLocaleString()))
     trEl.appendChild(createTd(data.followers))
+    trEl.appendChild(createTd(calcValueDelta(data.followers, prevFollowers)))
     trEl.appendChild(createTd(data.likes))
+    trEl.appendChild(createTd(calcValueDelta(data.likes, prevLikes)))
     $('#tracker tbody').prepend(trEl);
 }
 
 const socket = io();
-let prevVals = '';
+let prevVals = [0, 0];
 socket.emit('refresh_data');
 socket.on('data', (data) => {
     if (!data) return;
@@ -21,10 +39,13 @@ socket.on('data', (data) => {
     document.getElementById('followers').innerHTML = data.followers;
     document.getElementById('likes').innerHTML = data.likes;
 
-    const currentVals = data.followers + data.likes;
-    if (currentVals === prevVals) return;
+    const currentVals = [data.followers, data.likes];
+    const hasChange = currentVals.some((val, i) => {
+        return (val - prevVals[i]) > 0;
+    });
+    if (!hasChange) return;
 
-    updateTable(data)
+    updateTable(data, prevVals[0], prevVals[1])
     prevVals = currentVals;
 });
 
